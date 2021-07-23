516c3f572bc32f3bc3054039f23657b14ee46762
Cilans <cilans@pop-os.localdomain>
2021-07-23 15:44:42 +0530

    some

--- blog/blogApp/urls.py
+++ blog/blogApp/urls.py
@@ -17,4 +17,4 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
-]
+] 
